c526a44a29b00ffab7d80998d00fadf461127124
Bryan Alzate <bryanalzate007@gmail.com>
2023-03-05 21:15:49 -0500

    se crea html y css para estilos y se genera archivo compressfiles.py funcional en la terminal para comprimir imagenes

--- compressFiles.py
+++ compressFiles.py
@@ -1,1 +1,12 @@
 #nuevo proyecto compresor de archivos
+from PIL import Image
+import os
+
+folder = "/home/bryan/Documents/Images/"
+if __name__ == "__main__":
+    for filename in os.listdir(folder):
+        name, extension = os.path.splitext(folder + filename)
+
+        if extension in [".jpg",".jpeg",".png"]:
+            picture = Image.open(folder + filename)
+            picture.save (folder + "compressed_"+filename, optimize=True, quality=60)
